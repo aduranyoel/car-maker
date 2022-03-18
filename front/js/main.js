a66f3759c8bb78b63406a46e83b1ba72d9f2a934
Maday Concepcion <yoet92@gmail.com>
2022-03-18 11:47:30 -0300

    add cache images

--- front/js/main.js
+++ front/js/main.js
@@ -1,21 +1,34 @@
 const
     EG_VIN = 'JTDZN3EU0E3298500',
     colors = document.querySelector('.colors'),
-    chassis = document.querySelector('#chassis'),
     carContainer = document.querySelector('.car-container');
+let carsLoaded = {}, currentChassis = document.querySelector('#chassis');
 
 const COLORS_NAME = ['369369369499', '477477479499', '370449485499', '436475499499', '403424397499', '409387419499', '494389399499', '490433397499', '540499411499', '389374374499'];
 
 window.onload = () => {
     colors.innerHTML = COLORS_NAME.map(color => `<div><img data-color="${ color }" src="./img/colors/${ color }.png" alt=""></div>`).join``;
+    carsLoaded = COLORS_NAME.reduce((acc, color) => {
+        const img = document.createElement('img');
+        img.src = `./img/chassis/${ color }.png`;
+        img.setAttribute('data-color', color);
+        acc[color] = img;
+        return acc;
+    }, {});
 }
 
 colors.addEventListener('click', e => {
-    const clone = chassis.cloneNode(true);
-    carContainer.append(clone);
+    const selected = e.target;
+    const newColor = selected.getAttribute('data-color');
+    const currentColor = currentChassis.getAttribute('data-color');
 
-    const newColor = e.target.getAttribute('data-color');
-    chassis.src = `./img/chassis/${ newColor }.png`;
+    if(!newColor || currentColor === newColor) return;
+
+    const clone = currentChassis.cloneNode(true);
+    carContainer.removeChild(currentChassis);
+    currentChassis = carsLoaded[newColor];
+    carContainer.appendChild(currentChassis);
+    carContainer.appendChild(clone);
 
     setTimeout(() => clone.style.opacity = '0')
     setTimeout(() => carContainer.removeChild(clone), 1000);
