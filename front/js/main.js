8f9324a5dcf5460f6241918e76dd91ad313a4807
Maday Concepcion <yoet92@gmail.com>
2022-03-20 14:20:44 -0300

    add loader

--- front/js/main.js
+++ front/js/main.js
@@ -4,17 +4,13 @@ const
     carContainer = document.querySelector('.car-container');
 let carsLoaded = {}, currentChassis = document.querySelector('#chassis');
 
-const COLORS_NAME = ['369369369499', '477477479499', '370449485499', '436475499499', '403424397499', '409387419499', '494389399499', '490433397499', '540499411499', '389374374499'];
+const COLORS_NAME = ['477477479499', '369369369499', '370449485499', '436475499499', '403424397499', '409387419499', '494389399499', '490433397499', '540499411499', '389374374499'];
 
 window.onload = () => {
-    colors.innerHTML = COLORS_NAME.map(color => `<div><img data-color="${ color }" src="./img/colors/${ color }.png" alt=""></div>`).join``;
-    carsLoaded = COLORS_NAME.reduce((acc, color) => {
-        const img = document.createElement('img');
-        img.src = `./img/chassis/${ color }.png`;
-        img.setAttribute('data-color', color);
-        acc[color] = img;
-        return acc;
-    }, {});
+    Promise.all([
+        loadAvailableColors(),
+        loadChassisColors().then(() => currentChassis.src = carsLoaded[COLORS_NAME[0]].src)]
+    ).then(pageReady);
 }
 
 colors.addEventListener('click', e => {
@@ -22,7 +18,7 @@ colors.addEventListener('click', e => {
     const newColor = selected.getAttribute('data-color');
     const currentColor = currentChassis.getAttribute('data-color');
 
-    if(!newColor || currentColor === newColor) return;
+    if (!newColor || currentColor === newColor) return;
 
     const clone = currentChassis.cloneNode(true);
     carContainer.removeChild(currentChassis);
@@ -38,3 +34,45 @@ colors.addEventListener('click', e => {
 const restClient = (url, params) => fetch(`/api/${ url }?${ new URLSearchParams(params) }`).then(res => res.json());
 const getSpecs = (vin) => restClient('specs', { vin });
 const getImages = ({ make, model, color, trim, year }) => restClient('images', { make, model, color, trim, year });
+
+async function loadAvailableColors() {
+    try {
+        const blobs = await Promise.all(COLORS_NAME.map(color => fetch(`./img/colors/${ color }.png`).then(r => ({
+            blob: r.blob(),
+            color
+        }))));
+        for (const { blob, color } of blobs) {
+            const div = document.createElement('div');
+            const img = document.createElement('img');
+            img.src = URL.createObjectURL(await blob);
+            img.setAttribute('data-color', color);
+            div.appendChild(img);
+            colors.appendChild(div);
+        }
+    } catch (e) {
+        return Promise.reject();
+    }
+}
+
+async function loadChassisColors() {
+    try {
+        const blobs = await Promise.all(COLORS_NAME.map(color => fetch(`./img/chassis/${ color }.png`).then(r => ({
+            blob: r.blob(),
+            color
+        }))));
+        for (const { blob, color } of blobs) {
+            const img = document.createElement('img');
+            img.src = URL.createObjectURL(await blob);
+            img.setAttribute('data-color', color);
+            carsLoaded[color] = img;
+        }
+    } catch (e) {
+        return Promise.reject();
+    }
+}
+
+function pageReady() {
+    document.querySelector('.loader').style.opacity = '0';
+    document.querySelector('.lds-ripple').style.display = 'none';
+    setTimeout(() => document.body.classList.remove('loading'), 1000);
+}
