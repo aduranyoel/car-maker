c28df648c4cbbca4ba4d5318d757e3db6b1cf2bd
Maday Concepcion <yoet92@gmail.com>
2022-03-27 18:21:16 -0300

    add rims changes

--- front/js/main.js
+++ front/js/main.js
@@ -1,11 +1,10 @@
 const
     colors = document.querySelector('.colors'),
+    rims = document.querySelector('.rims'),
     carContainer = document.querySelector('.car-container'),
     drag = document.querySelector('.drag'),
     tiresElement = document.querySelector('#tires'),
     skinElement = document.querySelector('#skin'),
-    wheelLeftElement = document.querySelector('#wheel-left'),
-    wheelRightElement = document.querySelector('#wheel-right'),
 
     COLORS_NAME = [
         '477477479499', '369369369499',
@@ -13,6 +12,12 @@ const
         '403424397499', '409387419499',
         '494389399499', '490433397499',
         '540499411499', '389374374499'],
+
+    RIMS_TYPE = [
+        'ASTBLDIA5', 'ASTGB5',
+        'ASTSIL5LO', 'ENIGBLD',
+        'ENIGMBD', 'ENIGSS',
+    ],
 
     mockWheelLeftPositions = [
         { top: 187, left: 231 },
@@ -52,13 +57,16 @@ const
     ];
 
 let colorsLoaded = {},
+    rimsLoaded = {},
     tires = [],
     skin = [],
     wheelLeft = [],
     wheelRight = [],
     chassis = [],
     chassisElement = document.querySelector('#chassis'),
-    isGrabbing = false, position = 1, grabBegin = 0, lastRange = 0, lastClientX = 0;
+    wheelLeftElement = document.querySelector('#wheel-left'),
+    wheelRightElement = document.querySelector('#wheel-right'),
+    isGrabbing = false, position = 1, grabBegin = 0, lastRange = 0, lastClientX = 0, currentRims;
 
 class Wheel {
     constructor(top = 0, left = 0, item = null) {
@@ -71,9 +79,10 @@ class Wheel {
 window.onload = () => {
     Promise.all([
         loadAvailableColors(),
+        loadAvailableRims(),
         loadResource(getPictures.bind(this, 'tires')).then(res => tires = res),
         loadResource(getPictures.bind(this, 'chassis/skin')).then(res => skin = res),
-        loadWheels(),
+        loadWheels('ASTBLDIA5'),
         loadChassisColors(COLORS_NAME[0]).then(() =>  colorsLoaded[COLORS_NAME[0]] = chassis),
     ]).then(pageReady).catch(console.error);
 }
@@ -91,6 +100,25 @@ async function loadAvailableColors() {
             img.setAttribute('data-color', color);
             div.appendChild(img);
             colors.appendChild(div);
+        }
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
+
+async function loadAvailableRims() {
+    try {
+        const blobs = await Promise.all(RIMS_TYPE.map(type => fetch(`./img/rims/${ type }.webp`).then(r => ({
+            blob: r.blob(),
+            type
+        }))));
+        for (const { blob, type } of blobs) {
+            const div = document.createElement('div');
+            const img = document.createElement('img');
+            img.src = URL.createObjectURL(await blob);
+            img.setAttribute('data-type', type);
+            div.appendChild(img);
+            rims.appendChild(div);
         }
     } catch (e) {
         return Promise.reject(e);
@@ -117,20 +145,27 @@ async function loadChassisColors(color) {
     }
 }
 
-async function loadWheels() {
+async function loadWheels(type) {
     try {
         const [left, right] = await Promise.all([
-            loadResource(getPictures.bind(this, 'wheels/left')),
-            loadResource(getPictures.bind(this, 'wheels/right'))
+            loadResource(getPictures.bind(this, `wheels/${ type }/left`)),
+            loadResource(getPictures.bind(this, `wheels/${ type }/right`))
         ]);
         wheelLeft = left.map((img, i) => {
             const pos = mockWheelLeftPositions[i];
+            img.classList.add('wheels');
             return new Wheel(pos.top, pos.left, img);
         })
         wheelRight = right.map((img, i) => {
             const pos = mockWheelRightPositions[i];
+            img.classList.add('wheels');
             return new Wheel(pos.top, pos.left, img);
         })
+        currentRims = type;
+        rimsLoaded[type] = {
+            left: [...wheelLeft],
+            right: [...wheelRight],
+        }
     } catch (e) {
         return Promise.reject(e);
     }
@@ -191,13 +226,10 @@ function grabEnd() {
     lastRange = 0;
 }
 
-const absFloor = num => Math.abs(Math.floor(num));
+const absFloor = num => Math.abs(~~num);
 
-drag.addEventListener('mousedown', grabStart);
-document.addEventListener('mouseup', grabEnd);
-drag.addEventListener('dragstart', e => e.preventDefault());
-document.addEventListener('mousemove', ({ clientX }) => {
-    if (!isGrabbing) return;
+function animation3d({ clientX }) {
+    if (!isGrabbing || typeof clientX !== 'number') return;
 
     const step = drag.getBoundingClientRect().width / 16;
     const toRight = clientX > lastClientX;
@@ -207,11 +239,20 @@ document.addEventListener('mousemove', ({ clientX }) => {
 
     if (lastRange === range) return;
 
-    const diffRange = lastRange < range ? range - lastRange : lastRange - range;
+    const diffRange = Math.abs(range - lastRange);
     position = ((toRight ? position + diffRange : position - diffRange) + 16) % 16;
     lastRange = range;
     setCarPosition(position);
-})
+}
+
+drag.addEventListener('mousedown', grabStart);
+document.addEventListener('mouseup', grabEnd);
+document.addEventListener('mousemove', animation3d);
+drag.addEventListener('dragstart', e => e.preventDefault());
+
+drag.addEventListener('touchstart', e => grabStart({ clientX: e.touches.item(0).clientX }));
+document.addEventListener('touchend', grabEnd);
+document.addEventListener('touchmove', e => animation3d({ clientX: e.touches.item(0).clientX }));
 
 colors.addEventListener('click', async(e) => {
     const selected = e.target;
@@ -235,6 +276,45 @@ colors.addEventListener('click', async(e) => {
 
     setTimeout(() => clone.style.opacity = '0')
     setTimeout(() => carContainer.removeChild(clone), 1000);
+})
+
+rims.addEventListener('click', async (e) => {
+    const newRims = e.target.getAttribute('data-type');
+
+    if (!newRims || currentRims === newRims) return;
+
+    const loaded = rimsLoaded[newRims];
+    if (loaded) {
+        wheelLeft = loaded.left;
+        wheelRight = loaded.right;
+        currentRims = newRims;
+    } else {
+        await loadWheels(newRims);
+    }
+
+    const wheelLeftClone = wheelLeftElement.cloneNode(true);
+    const wheelRightClone = wheelRightElement.cloneNode(true);
+    carContainer.removeChild(wheelLeftElement);
+    carContainer.removeChild(wheelRightElement);
+    wheelLeftElement = wheelLeft[position].item.cloneNode(true);
+    wheelRightElement = wheelRight[position].item.cloneNode(true);
+    wheelLeftElement.style.left = wheelLeft[position].left + 'px';
+    wheelRightElement.style.left = wheelRight[position].left + 'px';
+    wheelLeftElement.style.top = wheelLeft[position].top + 'px';
+    wheelRightElement.style.top = wheelRight[position].top + 'px';
+    carContainer.appendChild(wheelLeftElement);
+    carContainer.appendChild(wheelRightElement);
+    carContainer.appendChild(wheelLeftClone);
+    carContainer.appendChild(wheelRightClone);
+
+    setTimeout(() => {
+        wheelLeftClone.style.opacity = '0'
+        wheelRightClone.style.opacity = '0'
+    })
+    setTimeout(() => {
+        carContainer.removeChild(wheelLeftClone)
+        carContainer.removeChild(wheelRightClone)
+    }, 1000);
 })
 
 // extra
